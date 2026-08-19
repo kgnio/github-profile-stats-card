aa312a99378b926202c0917a027e09a996da73d5
Ogulkagan MAMAK <kagandev@proton.me>
2026-08-19 17:52:27 +0300

    update: robots.ts updated for ai readability.

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,16 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+    ],
     sitemap: "https://kgnio-profile-card.vercel.app/sitemap.xml",
     host: "https://kgnio-profile-card.vercel.app",
   };
